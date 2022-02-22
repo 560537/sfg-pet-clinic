ca993082e1db36a1dc911e1e6cde342aff5fb5a8
Magda Mathews I560537 <magda.mathews@sap.com>
2022-02-22 10:15:39 +0100

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index"; // Thymeleaf will look for a template called index
     }
+
+    @RequestMapping("/oups")
+    public String oupsHanlder(){
+        return "notimplemented";
+    }
 }
